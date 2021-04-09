52aef7b184c1832dc1cfdcaade57f2b02daf9dd9
Scooby <redraidere99@gmail.com>
2021-04-09 15:10:01 -0400

    Multiplication table assignment.

---
+++ multiplicationTable/src/multiplicationTable/tableCode.java
@@ -0,0 +1,20 @@
+package multiplicationTable;
+
+public class tableCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for (int i = 1;
+			i<=10; i++){
+			
+			  for (int x = 1; x<=10; x++)
+				  
+	System.out.print("\t"+ i * x);
+    System.out.println(); 
+    
+	}
+		
+   }
+}
